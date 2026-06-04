a2be278c76ce01a43acb4be33f921eb392b1127a
Rohit Varma Datla <visaal1582892@gmail.com>
2026-06-04 15:05:11 +0530

    started new project

---
+++ agreement-tracker-backend/src/main/java/com/medplus/agreement_tracker_backend/AgreementTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medplus.agreement_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgreementTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgreementTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ agreement-tracker-backend/src/test/java/com/medplus/agreement_tracker_backend/AgreementTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medplus.agreement_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgreementTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
